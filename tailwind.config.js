e09c37a1dab29aa5041ba86480d08b49fd20c24d
Paria <paria.slmni@gmail.com>
2024-07-23 19:36:56 +0330

    landing(feat): responsive layout, style and script 🚀

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html', './src/*.js'],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    extend: {
+      colors: {
+        cyan: 'hsl(180, 66%, 49%)',
+        cyanLight: 'hsl(180, 66%, 69%)',
+        darkViolet: 'hsl(257, 27%, 26%)',
+        red: 'hsl(0, 87%, 67%)',
+        grayishViolet: 'hsl(257, 7%, 63%)',
+        veryDarkBlue: 'hsl(255, 11%, 22%)',
+        veryDarkViolet: 'hsl(260, 8%, 14%) ',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
